0df2c188fc11309bdd8fb80ca4b52077c771ab58
LorenzoACS <89931591+LorenzoACS@users.noreply.github.com>
2021-10-23 22:18:17 -0400

    Update BuffonSimulation.java

--- BuffonSimulation.java
+++ BuffonSimulation.java
@@ -2,13 +2,45 @@ import java.util.ArrayList;
 
 public class BuffonSimulation {
     public static Tarp tarp = new Tarp(400.0, 100.0);
-    public static ArrayList<Needle> needles;
-    public static Needle test = new Needle(100.0, tarp);
+    public static ArrayList<Needle> needles = new ArrayList<Needle>();
+    public static boolean bool = true;
+    public static int counter = 0;
+    public static int max = 150;
     
     public static void main(String[] args) {
-        System.out.println(test.angle());
-        System.out.println(test.center());
-        System.out.println(test.leftTip());
-        System.out.println(test.rightTip());
+        //System.out.println(test.angle());
+        //System.out.println(test.center());
+        //System.out.println(test.leftTip());
+        //System.out.println(test.rightTip());
+        for (int i = 0; i < max; i++) {
+            needles.add(new Needle(100.0, tarp));
+            //System.out.println(needles.size());
+            System.out.println(needles.get(i).leftTip() + " Left");
+            System.out.println(needles.get(i).rightTip() + " Right");
+            if (needles.get(i).leftTip() == 0.0 || needles.get(i).leftTip() < 0.0) {
+                counter++;
+            } 
+            if (needles.get(i).rightTip() == 0.0 || needles.get(i).rightTip() < 0.0) {
+                counter++;
+            } 
+            if (needles.get(i).leftTip() == 400.0 || needles.get(i).leftTip() > 400.0) {
+                counter++;
+            } 
+            if (needles.get(i).rightTip() == 400.0 || needles.get(i).rightTip() > 400.0) {
+                counter++;
+            }   
+            if (i + 1 == max) {
+                System.out.println(counter);
+                System.out.println(needles.size());
+                System.out.println(needles.size() / (double) counter);
+                //System.out.println(tarp.numberOfCrossings());
+                //System.out.println(tarp.fractionOfCrossings());
+                break;
+            }
+        }
     }
+        //System.out.println(tarp.numberOfCrossings());
+        //System.out.println(tarp.fractionOfCrossings());
+              
 }
+
